76e1838244640c33f1a24b8ad3ade7233c2f537f
yanndarkz <iansantosmcqueen@gmail.com>
2024-11-27 17:26:45 -0300

    feat: allVideos components

---
+++ src/app/pages/all-videos/all-videos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-videos',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-videos.component.html',
+  styleUrl: './all-videos.component.scss'
+})
+export class AllVideosComponent {
+
+}
