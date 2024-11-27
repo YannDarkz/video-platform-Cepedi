2c903d8ffe3244bde2d3b5115d45d6b542765bba
yanndarkz <iansantosmcqueen@gmail.com>
2024-11-26 23:46:36 -0300

    feature app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+import { LikeVideosComponent } from './pages/like-videos/like-videos.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SeeLaterComponent } from './pages/see-later/see-later.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { LoginComponent } from './pages/login/login.component';
+import { authGuard } from './guards/auth/auth.guard';
+import { SingleVideoComponent } from './pages/single-video/single-video.component';
+import { AllVideosComponent } from './pages/all-videos/all-videos.component';
+
+
+
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+    { path: 'like-videos', component: LikeVideosComponent, canActivate: [authGuard] },
+    { path: 'see-later', component: SeeLaterComponent, canActivate: [authGuard] },
+    { path: 'favorites', component: FavoritesComponent, canActivate: [authGuard] },
+    { path: 'videos', component: AllVideosComponent, canActivate: [authGuard] },
+    { path: 'videos/:id', component: SingleVideoComponent, canActivate: [authGuard] },
+
+
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+
+export class AppRoutingModule { }
