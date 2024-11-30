e6e114d56f7781f4e15101b58098403ae5127c2a
yanndarkz <iansantosmcqueen@gmail.com>
2024-11-29 23:29:39 -0300

    feature: all-videos component

--- src/app/pages/all-videos/all-videos.component.ts
+++ src/app/pages/all-videos/all-videos.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { VideoService } from '../../services/videoService/video.service';
+import { IVideo } from '../../interfaces/video.model';
 
 @Component({
   selector: 'app-all-videos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './all-videos.component.html',
   styleUrl: './all-videos.component.scss'
 })
 export class AllVideosComponent {
+  videos: IVideo[] = [];
+
+  constructor(private videoService: VideoService) {}
+
+  handleClick(event: Event): void {
+    event.preventDefault();
+   
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {this.videoService.getVideos().subscribe((data) => (this.videos = data));}, 3000 )
+    // this.videoService.getVideos().subscribe((data) => (this.videos = data));
+  }
 
 }
