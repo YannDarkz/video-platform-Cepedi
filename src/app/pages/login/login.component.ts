6605ceeae3e75b7d41ba654c855712756a3a5f6f
yanndarkz <iansantosmcqueen@gmail.com>
2024-11-29 23:31:52 -0300

    feat: login.ts

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,16 +12,32 @@ import { CommonModule } from '@angular/common';
 })
 export class LoginComponent {
   auth: AuthService;
-  constructor(private authP: AuthService) { 
+  constructor(private authP: AuthService, private router: Router) {
     this.auth = authP
   }
+
+  ngOnInit() {
+    this.auth.isAuthenticated$.subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        this.router.navigate(['/home']);
+        // if (isAuthenticated) {
+        //   if (this.router.url === '/home') {
+        //     return; 
+        //   }
+        // }
+      }
+    })
+
+
+  }
+
 
   loginWithRedirect(): void {
     this.auth.loginWithRedirect();
   }
 
   logout(): void {
-    this.auth.logout({logoutParams: { returnTo: window.location.origin} })
+    this.auth.logout({ logoutParams: { returnTo: window.location.origin } })
   }
 
 }
